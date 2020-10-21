f9d8e2efcbb2b56d23c5478370719e6d9ab98bf3
fenghaohaha <fenghaohaha>
2020-10-21 16:09:23 +0800

    latch

--- src/main/java/com/example/concurrency/features/countdownlatch/CountDownLatchEx.java
+++ src/main/java/com/example/concurrency/features/countdownlatch/CountDownLatchEx.java
@@ -14,6 +14,7 @@ import java.util.concurrent.Executor;
  */
 public class CountDownLatchEx {
     public static void main(String[] args) throws InterruptedException{
+        System.out.println();
         // 创建 2 个线程的线程池
         Executor executor = ThreadPoolBuilder.fixedPool().setPoolSize(2).build();
         // 计数器初始化为 2
@@ -26,9 +27,14 @@ public class CountDownLatchEx {
             System.out.println("T2");
             latch.countDown();
         });
+        executor.execute(()-> {
+            System.out.println("T3");
+            latch.countDown();
+        });
         // 等待两个查询操作结束
         latch.await();
 
+        System.out.println("main thread continue");
 
     }
 }
--- src/main/java/com/example/concurrency/features/cyclicbarrier/CyclicBarrierEx.java
+++ src/main/java/com/example/concurrency/features/cyclicbarrier/CyclicBarrierEx.java
@@ -27,7 +27,7 @@ public class CyclicBarrierEx {
                 cyclicBarrier.await();
                 System.out.println(Thread.currentThread().getName() + "开始执行");
                 // 工作线程开始处理，这里用Thread.sleep()来模拟业务处理
-                Thread.sleep(1000);
+                Thread.sleep(10000);
                 System.out.println(Thread.currentThread().getName() + "执行完毕");
             } catch (Exception e) {
                 e.printStackTrace();
@@ -35,8 +35,8 @@ public class CyclicBarrierEx {
         }
     }
     public static void main(String[] args) {
-        int threadCount = 3;
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(threadCount);
+        int threadCount = 4;
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(threadCount - 1);
         for (int i = 0; i < threadCount; i++) {
             System.out.println("创建工作线程" + i);
             Worker worker = new Worker(cyclicBarrier);
