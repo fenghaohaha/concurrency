c44b691f654c388dde2a587be85f4f3798957c94
fenghaohaha <fenghaohaha>
2020-11-04 15:01:18 +0800

    thread Exchange

--- src/main/java/com/example/concurrency/features/exchanger/ExchangerExample.java
+++ src/main/java/com/example/concurrency/features/exchanger/ExchangerExample.java
@@ -9,6 +9,8 @@ import java.util.concurrent.ThreadPoolExecutor;
  * 描述:
  * 交换者 用于两个线程间的数据交换
  *
+ * 只能是两个线程，不支持更多的线程，当线程A调用exchange方法后，会陷入到阻塞状态，
+ * 直到线程B也调用了exchange方法，然后两者以线程安全的方式交换数据，各自继续运行
  * @author zed
  * @since 2019-07-03 5:39 PM
  */
@@ -20,7 +22,8 @@ public class ExchangerExample {
         poolExecutor.execute(()->{
             try{
                 String s ="SomethingAndA";
-                EXCHANGER.exchange(s);
+                String s1 = EXCHANGER.exchange(s);
+                System.out.println("thread 1  s1 = " + s1);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
@@ -28,8 +31,9 @@ public class ExchangerExample {
         poolExecutor.execute(()->{
             try{
                 String s1 = "SomethingAndB";
-                String s = EXCHANGER.exchange("s1");
-                System.out.println("s和s1值是否相等："+s1.equals(s)+",s："+s+",s1:"+s1);
+                Thread.sleep(10000);
+                String s = EXCHANGER.exchange(s1);
+                System.out.println("s和s1值是否相等：" + s1.equals(s) + ",s：" + s + ",s1:" + s1);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
